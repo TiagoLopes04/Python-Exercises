07c53ca613ebaf51cda3a9f8f5826fe459fbc7bf
TiagoLopesA54955 <a54955@alunos.ipb.pt>
2025-03-19 19:56:22 +0000

    Password Generator v1

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -6,24 +6,24 @@ def generate_password():
     numbers = string.digits
     symbols = string.punctuation
 
-    password_list = []  # Store characters in a list instead of a string
+    password_list = []
 
     number_of_letters = random.randint(5, 10)
     number_of_symbols = random.randint(5, 10)
     number_of_numbers = random.randint(5, 10)
 
-    for _ in range(number_of_letters):
+    for i in range(number_of_letters):
         password_list.append(random.choice(letters))
 
-    for _ in range(number_of_symbols):
+    for i in range(number_of_symbols):
         password_list.append(random.choice(symbols))
 
-    for _ in range(number_of_numbers):
+    for i in range(number_of_numbers):
         password_list.append(random.choice(numbers))
 
-    random.shuffle(password_list)  # Shuffle the list
+    random.shuffle(password_list)
 
-    password = "".join(password_list)  # Convert list back to a string
+    password = "".join(password_list)
     print(password)
 
 generate_password()
